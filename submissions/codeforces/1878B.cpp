09e32e279333f28472ef07714eac2172124d89a8
Christian Fleury <50469574+chfleury@users.noreply.github.com>
2023-09-26 12:35:42 -0300

    Create 1878B.cpp

---
+++ submissions/codeforces/1878B.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+void solve()
+{
+    bool ans = false;
+    int n;
+    cin >> n;
+    int t;
+    t = 1;
+    for (int i = 0; i < n; i++)
+    {
+        t += 2;
+        cout << t << " ";
+        }
+
+    cout << endl;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
